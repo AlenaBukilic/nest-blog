f6bcf58ac7262800cf1f521fbc55c22eed78d947
Alena Bukilic <alena.bukilic@florencehc.com>
2023-06-07 19:29:37 +0200

    wtf

--- src/user/service/user.service.ts
+++ src/user/service/user.service.ts
@@ -7,6 +7,7 @@ import { supportedRoles, UserPublic } from '../models/user.interface';
 import { User, UserDocument } from '../models/user.schema';
 import { paginate, PaginationOptions } from 'nestjs-paginate-mongo';
 import { PaginationData, SearchQuery } from '../../types/types.exporter';
+console.log('');
 
 @Injectable()
 export class UserService {
